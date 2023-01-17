55dfad55ad173afeea80635a2c584432925650dd
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-01-17 08:59:02 +0100

    feat: adding update user endpoint

--- src/routes/user_route.py
+++ src/routes/user_route.py
@@ -1,9 +1,10 @@
+from bson import ObjectId
 from fastapi import APIRouter
 
 from ..models.user import User
 from ..configs.db import conn
 
-from ..schemas.user_schema import users_entities_schema
+from ..schemas.user_schema import users_entities_schema, user_schema
 
 user_route = APIRouter(prefix="/api/v1", tags=["Users"])
 
@@ -19,3 +20,10 @@ async def create_user(user: User):
     """create new user"""
     conn.local.user.insert_one(dict(user))
     return users_entities_schema(conn.local.user.find())
+
+
+@user_route.put("/update/{id}")
+async def update_user(id: str, user: User):
+    """update user"""
+    conn.local.user.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(user)})
+    return user_schema(conn.local.user.find_one({"_id": ObjectId(id)}))
